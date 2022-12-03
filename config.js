602ee85627c84e2149cbb95bdba845e6b245eae8
Kiloppm <118220209+Kiloppm@users.noreply.github.com>
2022-12-03 21:36:11 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,9 +7,9 @@ global.autoReadAll = false
 global.antitags = true
 
 
-global.Owner = ['918101187835']
-global.OwnerNumber = ['918101187835']
-global.ownertag = ['918101187835']
+global.Owner = ['212645830816']
+global.OwnerNumber = ['212645830816']
+global.ownertag = ['212645830816']
 global.BotName = "Miku"
 global.packname = "Miku bot"
 global.author = "By: Fantox"
